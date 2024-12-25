70cb9da3b6af447384894362bb72cbd265c7e887
gaffey <Songyjgaf@163.com>
2024-12-25 12:10:48 +0800

    项目初始化

---
+++ CloudPicture/src/main/java/com/cloudpic/cloudpicture/CloudPictureApplication.java
@@ -0,0 +1,13 @@
+package com.cloudpic.cloudpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPictureApplication.class, args);
+	}
+
+}
