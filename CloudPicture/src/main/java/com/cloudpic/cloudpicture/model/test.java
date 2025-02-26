31f80fcc20f2757374fd1a13dda3fdb4f8a05970
gaffey <Songyjgaf@163.com>
2025-02-26 11:17:48 +0800

    11

---
+++ CloudPicture/src/main/java/com/cloudpic/cloudpicture/model/test.java
@@ -0,0 +1,12 @@
+package com.cloudpic.cloudpicture.model;
+
+/**
+ * @CLass: test
+ * @BelongProject:CloudPicture
+ * @Author: gaf_Song
+ * @Date:2025/2/26
+ * @Version: 1.0
+ * @Description:
+ */
+public class test {
+}
